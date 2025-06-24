ef7d1f1228e2bffbba5558166e1f5c48bb2c5644
rishitjindal2 <rishitjindal2@outlook.com>
2025-06-24 11:49:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/finance-hub2/', // <-- important
+  plugins: [react()],
+})
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
